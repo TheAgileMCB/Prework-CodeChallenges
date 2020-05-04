1cb1ff2b86880fcd40b0bb50a786c0c3cccd6a27
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-03 23:29:06 -0500

    working through challenge 1

---
+++ Challenges/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array of int and user int
+            int[] numbers = new int[5];
+
+            // request 5 int from 1-10
+            for (int i = 0; i > 6; i ++)
+            {
+                Console.WriteLine("Please enter a number from 1 - 10.");
+                numbers[i] = int.Parse(Console.ReadLine());
+                if (numbers[i] <= 10)
+                {
+                    
+                }
+            }
+
+           // output the array of 5 int
+           Console.WriteLine();
+
+            // user selects 1
+            int.Parse(Console.ReadLine());
+
+            // score = int * frequency of int
+
+            // output "score"
+            Console.WriteLine();
+
+           // compose readme
+
+        }
+    }
+}
