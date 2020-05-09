1f75c9b2d46597318bcf9211d7975156ca30135c
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-09 15:26:46 -0500

    accomplished challenge 1

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -10,25 +10,42 @@ namespace Challenges
             int[] numbers = new int[5];
 
             // request 5 int from 1-10
-            for (int i = 0; i < numbers.Length; i ++)
+            for (int i = 0; i < numbers.Length; i++)
             {
                 Console.WriteLine("Please enter a number from 1 - 10.");
                 numbers[i] = int.Parse(Console.ReadLine());
             }
-            
-           // output the array of 5 int
-           Console.WriteLine(string.Join(",", numbers));
+
+            // output the array of 5 int
+            Console.WriteLine(string.Join(",", numbers));
 
             // user selects 1
-            int.Parse(Console.ReadLine());
+            Console.WriteLine("Now, choose one these values.");
+            int userValue = int.Parse(Console.ReadLine());
 
-            // score = int * frequency of int
+            int result = ScoreCalculator(userValue, numbers);
 
             // output "score"
-            Console.WriteLine();
+            Console.WriteLine($"Your score is {result}");
 
-           // compose readme
-          
+            // compose readme
+
+        }
+
+        // score = int * frequency of int
+        static int ScoreCalculator(int userValue, int[] numbers)
+        {
+            int scoreCounter = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (userValue == numbers[i])
+                {
+                    scoreCounter++;
+                }
+            }
+
+            int result = userValue * scoreCounter;
+            return result;
         }
     }
 }
